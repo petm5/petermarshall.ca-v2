ec061b5f6088c4ce634f97219e2134e495a0c6f8
Peter Marshall <petms@proton.me>
2025-06-02 16:38:54 -0400

    add image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		imagetools({
+			defaultDirectives: (url) => {
+				return new URLSearchParams({
+					w: 800,
+					format: 'avif',
+					effort: 2,
+					quality: 60
+				})
+			}
+		})
+	]
 });
