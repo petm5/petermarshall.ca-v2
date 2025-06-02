db4bab7686897a9e046f85351f02caa6b3affc92
Peter Marshall <petms@proton.me>
2025-06-02 16:38:37 -0400

    fix blog ui

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,15 +5,13 @@
 {#if data.posts.length > 0}
 <h1>Blog</h1>
 
-<ul>
+<ul class="posts">
 	{#each data.posts as post}
 		<li>
-			<h2>
-				<a href={post.path}>
-					{post.meta.title}
-				</a>
-			</h2>
-			Published {post.meta.date}
+			<a href={post.path}>
+				<h2>{post.meta.title}</h2>
+				<p><small class="date">{post.meta.date}</small></p>
+			</a>
 		</li>
 	{/each}
 </ul>
@@ -21,3 +19,18 @@
 <h1>Nothing Here Yet...</h1>
 <p>Looks like nothing was found. Please check back later.</p>
 {/if}
+
+<style>
+	.posts {
+		list-style: none;
+		padding: 0;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 2.8em 2rem;
+
+		& a {
+			color: inherit;
+			text-decoration: none;
+		}
+	}
+</style>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,6 @@
 	export let data;
 </script>
 
-<article>
-	<h1>{data.title}</h1>
-	<p>Published: {data.date}</p>
-	<svelte:component this={data.content} />
-</article>
+<h1>{data.title}</h1>
+<p>Published: {data.date}</p>
+<svelte:component this={data.content} />
